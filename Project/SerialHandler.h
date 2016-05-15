555dc05abd4311efb52ec061eec59f789c4581a3
xrgman <xrgman@gmail.com>
2016-05-15 22:29:28 +0200

    Added RS232 Serial handler, basic send and receive functions.

--- Project/SerialHandler.h
+++ Project/SerialHandler.h
@@ -1,15 +1,27 @@
 #ifndef SERIALHANDLER_H
 #define SERIALHANDLER_H
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 #include <string>
+#include "rs232.h"
+
 
 class SerialHandler {
 	private:
-
+		bool connected;
+		bool initializeCommPort();
+		int commPortNumber;
 	public:
-		SerialHandler(std::string commPort);
+		SerialHandler(int commPortNumber);
+		~SerialHandler();
 		void sendCommand(std::string);
+		
 		std::string receiveCommand();
+		bool isConnected();
 };
 
 #endif
