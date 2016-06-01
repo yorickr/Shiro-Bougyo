638976f357f0a958ce20c3f35bce91bcb8b7bba4
rene1997 <renekeijzer1997@gmail.com>
2016-06-01 15:49:57 +0200

    zwaartekracht verbeterd en begin tweede warrior

---
+++ Project/SecondWarriorModel.cpp
@@ -0,0 +1,28 @@
+#include "SecondWarriorModel.h"
+
+
+SecondWarriorModel::SecondWarriorModel(float x, float z): CollisionModel("")
+{
+	xpos = x;
+	zpos = z;
+	ypos = -3;
+	boundingSpheres.clear();
+	SecondWarriorModel::InitBoundingSpheres();
+}
+
+SecondWarriorModel::~SecondWarriorModel()
+{
+}
+
+void SecondWarriorModel::update(float deltatime)
+{
+
+}
+
+void SecondWarriorModel::InitBoundingSpheres()
+{
+}
+
+void SecondWarriorModel::setSize(int)
+{
+}
