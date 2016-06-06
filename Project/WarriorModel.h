c0d771ee6f1cac89cedd24cf7b3287f16b117d92
xrgman <xrgman@gmail.com>
2016-06-06 13:05:03 +0200

    Added dead animation

--- Project/WarriorModel.h
+++ Project/WarriorModel.h
@@ -1,15 +1,18 @@
 #pragma once
 
+#include "GameState.h"
 #include "CollisionModel.h"
 #include "Player.h"
 
 enum class WarriorType {first = 0 , second = 1};
 
+class GameState;
+
 class WarriorModel :
 	public CollisionModel
 {
 public:
-	WarriorModel(float x, float z, WarriorType type, string filename);
+	WarriorModel(float x, float z, WarriorType type, string filename, GameState *game);
 	~WarriorModel();
 	void update(float deltatime) override;
 	void InitBoundingSpheres() override;
@@ -17,10 +20,12 @@ public:
 	void setSize(int);
 	bool removeHealth(Player* player);//returns true if health <= 0 otherwise false
 	void setPosition(int, int, int);
+	void setRotation(int, int, int);
+private:
 	int health = 100;
-
-private:	
 	WarriorType warriortype;
 	short right;
+	int isDead;
+	GameState *game;
 };
 
